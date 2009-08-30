c29e0fcc34fc92c4737d8020fb7ee1e6d50df4d9
morcsik <morcsik@COST-D-040.localdomain>
2009-08-30 11:51:22 +0200

    * new work for url handling objects

---
+++ lib/controllers/requests/vschttprequesta.class.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * @package vsc_controllers
+ * @subpackage requests
+ * @author marius orcsik <marius@habarnam.ro>
+ * @date 09.07.13
+ */
+abstract class vscHttpRequestA {
+	private $sHttpMethod = null;
+	private $aGetVars = array();
+	private $aPostVars = array();
+	private $aCookieVars = array();
+	private $aSessionVars = array();
+	private $sServerProtocol = null;
+
+	private $aVarOrder;
+
+	public function __construct () {
+		$this->aGetVars = $_GET;
+		$this->aPostVars = $_POST;
+		$this->aCookieVars = $_COOKIE;
+//		$this->aSessionVars = $_SESSION;
+
+		if (isset($_SERVER)) {
+			$this->getServerProtocol();
+			$this->getHttpMethod();
+		}
+	}
+
+	public function getServerProtocol () {
+		if (!$this->sServerProtocol) {
+			$this->sServerProtocol = $_SERVER['SERVER_PROTOCOL'];
+		}
+
+		return $this->sServerProtocol;
+	}
+
+	public function getVarOrder () {
+		if (count($this->aVarOrder) != 4){
+			// get gpc order
+			$sOrder = ini_get('variables_order');
+			for ($i = 0; $i < 4; $i++) {
+				// reversing the order
+				$this->aVarOrder[$i] = substr($sOrder, $i, 1);
+			}
+		}
+		return $this->aVarOrder;
+	}
+
+	public function getVar ($sVarName) {
+		foreach ($this->getVarOrder() as $sMethod) {
+			try {
+				switch ($sMethod) {
+					case 'G':
+						return $this->getGetVar($sVarName);
+						break;
+					case 'P':
+						return $this->getPostVar($sVarName);
+						break;
+					case 'C':
+						return $this->getCookieVar($sVarName);
+						break;
+					case 'S':
+	//					return $this->getSeesionVar($sVarName);
+						break;
+				}
+			} catch (vscException $e) {
+				// no var - go on
+			}
+		}
+		throw new vscException ('Variable ' . $sVarName . ' doesn\'t exist in the http request.');
+	}
+
+	protected function getGetVar ($sVarName) {
+		if (key_exists($sVarName, $this->aGetVars))
+			return $this->aGetVars[$sVarName];
+		else throw new vscException ('No GET variable named: ' . $sVarName);
+	}
+
+	protected function getPostVar ($sVarName) {
+		if (key_exists($sVarName, $this->aPostVars))
+			return $this->aPostVars[$sVarName];
+		else throw new vscException ('No POST variable named: ' . $sVarName);
+	}
+
+	protected function getCookieVar ($sVarName) {
+		if (key_exists($sVarName, $this->aCookieVars))
+			return $this->aCookieVars[$sVarName];
+		else throw new vscException ('No COOKIE variable named: ' . $sVarName);
+	}
+	public function setCookieVar ($sVarName, $sVarValue) {
+		// TODO
+	}
+
+	public function getHttpMethod () {
+		if (!$this->sHttpMethod) {
+			$this->sHttpMethod = $_SERVER['REQUEST_METHOD'];
+		}
+		return $this->sHttpMethod;
+	}
+
+	public function isGet() {
+		return ($this->getHttpMethod() == 'GET');
+	}
+
+	public function isPost() {
+		return ($this->getHttpMethod() == 'POST');
+	}
+
+}
---
+++ lib/controllers/requests/vscrwhttprequest.class.php
@@ -0,0 +1,82 @@
+<?php
+
+class vscRwHttpRequest extends vscHttpRequestA {
+	private $sRequestUri = null;
+	private $aTaintedVars = array();
+
+	public function __construct () {
+		parent::__construct();
+		if (isset ($_SERVER)) {
+			$this->getRequestUri();
+			$this->constructTaintedVars ();
+		}
+	}
+
+	protected function getTaintedVar ($sVarName) {
+		if (key_exists($sVarName, $this->aTaintedVars))
+			return $this->aTaintedVars[$sVarName];
+		else throw new vscException ('No URL variable named: ' . $sVarName);
+	}
+
+	public function getVar ($sVarName) {
+		try {
+			return parent::getVar($sVarName);
+		} catch (vscException $e) {
+			try {
+				return $this->getTaintedVar($sVarName);
+			} catch (vscException $e) {
+				throw new vscException ('Variable ' . $sVarName . ' doesn\'t exist in the HTTP request or in the URL.');
+			}
+		}
+	}
+
+	/**
+	 * Returns the REQUEST_URI which is used to get the URL Rewrite variables
+	 * This will also remove the part of the path that is actually an existing path
+	 * lighttpd:
+	 *  url.rewrite = (
+	 * 		"^/([^?]*)?(.*)$" => "/index.php$2"
+ 	 *  )
+	 *
+	 * @todo move to the vscUrlRWParser
+	 * @return string
+	 */
+	public function getRequestUri () {
+		if (!$this->sRequestUri) {
+			$sServerType = $_SERVER['SERVER_SOFTWARE'];
+
+			// this header is present for all server in the same form
+			$sCurrentScriptDir = dirname($_SERVER['PHP_SELF']) != '/' ? dirname($_SERVER['PHP_SELF']) : '';
+			if (stristr($sServerType, 'lighttpd')) {
+				$sReqUri = $_SERVER['REQUEST_URI'];
+				$this->sRequestUri = str_replace ($sCurrentScriptDir, '', $sReqUri);
+			} elseif (stristr($sServerType, 'apache')) {
+				$sReqUri = $_SERVER['REQUEST_URI'];
+				$this->sRequestUri = str_replace ($sCurrentScriptDir, '', $sReqUri);
+			} elseif (stristr($sServerType, 'cherokee')) {
+				// TODO
+			}
+
+			// removing unnecessary get vars
+			$iQMarkPos = strpos ($this->sRequestUri, '?');
+			if ($iQMarkPos) {
+				$this->sRequestUri = substr ($this->sRequestUri, 0, $iQMarkPos);
+			}
+		}
+		return $this->sRequestUri;
+	}
+
+	// this has to be moved in the rw url handler
+	public function constructTaintedVars () {
+		foreach(explode ('/', $this->getRequestUri()) as $sUrlId) {
+			if ($sUrlId) {
+				$t = explode (':', $sUrlId);
+				if (count($t) > 1) {
+					$this->aTaintedVars[array_shift($t)] = implode(':', $t);
+				} else {
+					$this->aTaintedVars[$t[0]] = '';
+				}
+			}
+		}
+	}
+}
