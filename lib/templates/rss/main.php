5662bce8216653708819dc638144c3f05abcf53c
morcsik <morcsik@COST-D-040.localdomain>
2009-08-31 16:21:04 +0200

    * changed the templates to include valid markup, ie files will be nested, not juxtaposed

---
+++ lib/templates/rss/main.php
@@ -0,0 +1,13 @@
+<?php echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<atom:link href="<?php echo $this->getUrl() ?>" rel="self" type="application/rss+xml" />
+		<title><?php echo $this->getTitle() ?></title>
+		<link><?php echo $this->getUrl() ?></link>
+		<description><?php echo $this->getDescription() ?></description>
+		<language><?php echo $this->getLanguage() ?></language>
+		<lastBuildDate><?php echo $this->getLastBuildDate() ?></lastBuildDate>
+		<docs>http://blogs.law.harvard.edu/tech/rss</docs>
+		<?php echo $this->getContent() ?>
+	</channel>
+</rss>
---
+++ lib/templates/xhtml/main.php
@@ -0,0 +1,31 @@
+<?php echo '<?xml version="1.0" encoding="utf-8"?>'; ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
+	<title><?php echo $this->getTitle(); ?></title>
+	<style type="text/css">
+<?php
+foreach ($this->getStyleSheets () as $aStyleSheet) {
+?> 
+		@import url(<?php echo $aStyleSheet['path']; ?>) <?php echo ($aStyleSheet['medium'] ?  $aStyleSheet['medium'] : '') ?> 
+<?php 
+}
+?>
+	</style>
+<?php foreach ($this->getMetaHeaders() as $aMeta) { ?>
+	<meta <?php
+	foreach ($aMeta as $sName => $sValue) {
+		echo $sKey .'="'.$sValue.'"';
+	} 
+	?> /> 
+<?php } ?>
+</head>
+<body>
+<?php $this->getContent() ?>
+</body>
+<?php foreach ($this->getScripts() as $aScript) { ?>
+	<script type="text/javascript" src="<?php echo $aScript['path'];?>" />
+<?php } ?>
+</html>
