e17c35dace6784221d08efda4f4bf54852445c5b
morcsik <morcsik@COST-D-040.localdomain>
2009-08-31 13:57:54 +0200

    * added an abstract dispatch and made the existing one into something url rw dispatch

---
+++ lib/presentation/dispatchers/vscdispatchera.class.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Parses the current request into a valid Front Controller / Controller pair
+ * @package vsc_presentation
+ * @subpackage dispatchers
+ * @author marius orcsik <marius@habarnam.ro>
+ * @date 09.08.31
+ */
+abstract class vscDispatcherA {
+	/**
+	 * @var vscHttpRequestA
+	 */
+	private $oRequest;
+	/**
+	 * @var vscSiteMap
+	 */
+	private $oSiteMap;
+
+	/**
+	 * @return vscFrontControllerA
+	 */
+	abstract public function getFrontController ();
+
+	/**
+	 * @return vscControllerA
+	 */
+	abstract public function getProcessController ();
+
+	/**
+	 *
+	 * @param string $sIncPath
+	 * @throws vscExceptionPath
+	 * @return void
+	 */
+	abstract public function loadSiteMap ($sIncPath);
+
+	public function getRequest () {
+		if (!($this->oRequest instanceof vscHttpRequestA))
+			$this->oRequest = vsc::getHttpRequest();
+		return $this->oRequest;
+	}
+}
---
+++ lib/presentation/dispatchers/vscrwdispatcher.class.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Parses the current request into a valid Front Controller / Controller pair
+ * @package vsc_presentation
+ * @subpackage dispatchers
+ * @author marius orcsik <marius@habarnam.ro>
+ * @date 09.07.10
+ */
+class vscRwDispatcher {
+	/**
+	 *
+	 * @var vscHttpRequestA
+	 */
+	private $oRequest;
+	/**
+	 * @var vscSiteMap
+	 */
+	private $oSiteMap;
+
+	public function __construct (){
+		$this->oRequest = vsc::getHttpRequest();
+	}
+
+	/**
+	 *
+	 * @return vscFrontController
+	 */
+	public function getFrontController () {
+		import ('presentation/controllers');
+		return new vscHtmlFrontController ();
+	}
+
+	public function getProcessController () {
+		return new tsMainController ();
+	}
+
+	/**
+	 *
+	 * @param string $sIncPath
+	 * @throws vscExceptionPath
+	 * @return void
+	 */
+	public function loadSiteMap ($sIncPath) {
+		if (!is_file ($sIncPath)) {
+			throw new vscExceptionPath('[' . $sIncPath . '] is not a valid path.');
+		} else {
+			import ('presentation/sitemaps');
+			$this->oSiteMap = new vscSiteMap ();
+			$this->oSiteMap->addEntry ('/', $sIncPath);
+		}
+	}
+
+	public function getRequest () {}
+}
