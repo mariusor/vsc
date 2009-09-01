b213b2f444b1500e38ae7b7944874783b63dc45d
Marius Orcsik <marius@habarnam.ro>
2009-09-01 15:11:49 +0200

    * changed the wording on the vsc addon file for snaptest

--- unit_testing/vsc_v2_environment.php
+++ unit_testing/vsc_v2_environment.php
@@ -2,12 +2,13 @@
 /**
  * this will be linked from the snaptest/addons folder
  */
-$config['name'] = 'Vsc addon';
+$config['name'] = 'VSC v.2';
 $config['version'] = '0.1';
-$config['author'] = 'hab';
-$config['description'] = '';
+$config['author'] = 'marius orcsik <marius@habarnam.ro>';
+$config['description'] = 'VSC v.2 configuration for UnitTesting';
 include_once ('core/reporter/reporter.php');
 include_once ('core/reporter/reporters/text.php');
 include_once ('core/reporter/reporters/phpserializer.php');
+
 set_include_path (realpath ('../vsc_v2/') . PATH_SEPARATOR . get_include_path());
 include ('vsc.inc.php');
