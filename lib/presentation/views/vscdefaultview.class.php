7ee0f1cd3eb34472f62045e4d4b4a5ea708864b2
Marius Orcsik <marius@habarnam.ro>
2009-09-08 17:03:52 +0200

    * forgot about the empty default view

--- lib/presentation/views/vscdefaultview.class.php
+++ lib/presentation/views/vscdefaultview.class.php
@@ -3,6 +3,6 @@
  * @package vsc_presentation
  * @subpackage views
  * @author marius orcsik <marius@habarnam.ro>
- * @date 09.08.30
+ * @date 09.09.07
  */
-abstract class vscViewA {}
+class vscDefaultView extends vscViewA {}
