1464b87b184eb550a41b7dd6747dfc1cc00102f7
Habar Nam <habarnam@hoffa.dreamhost.com>
2009-09-08 04:39:08 -0700

    * changes to the snaptest vsc addon, to reflect the default git checkout directory

--- unit_testing/vsc_v2_environment.php
+++ unit_testing/vsc_v2_environment.php
@@ -10,5 +10,5 @@ include_once ('core/reporter/reporter.php');
 include_once ('core/reporter/reporters/text.php');
 include_once ('core/reporter/reporters/phpserializer.php');
 
-set_include_path (realpath ('../vsc_v2/') . PATH_SEPARATOR . get_include_path());
+set_include_path (realpath ('../vsc-v.2/') . PATH_SEPARATOR . get_include_path());
 include ('vsc.inc.php');
