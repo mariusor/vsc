16947a473d275020d06a51bfbf49f409914050f7
Marius Orcsik <marius@habarnam.ro>
2010-03-30 15:03:06 +0200

    * wrong function

--- lib/infrastructure/vscobject.class.php
+++ lib/infrastructure/vscobject.class.php
@@ -7,18 +7,18 @@
 import (VSC_LIB_PATH . 'exceptions');
 abstract class vscObject {
 	public function __call ($sMethodName, $aVars) {
-		throw new vscExceptionUnimplemented ('Method [' . gettype($this) .'::' . $sMethodName .'] not implemented for calling.');
+		throw new vscExceptionUnimplemented ('Method [' . get_class($this) .'::' . $sMethodName .'] not implemented for calling.');
 	}
 
 	public function __callStatic ($sMethodName, $aVars) {
-		throw new vscExceptionUnimplemented ('Method [' . gettype($this) .'::' . $sMethodName .'] not implemented for calling statically.');
+		throw new vscExceptionUnimplemented ('Method [' . get_class($this) .'::' . $sMethodName .'] not implemented for calling statically.');
 	}
 
 	public function __get ($sVarName) {
-		throw new vscExceptionUnimplemented ('Property [' . gettype($this) .'::' . $sVarName .'] not implemented for reading.');
+		throw new vscExceptionUnimplemented ('Property [' . get_class($this) .'::' . $sVarName .'] not implemented for reading.');
 	}
 
 	public function __set ($sVarName, $mValue) {
-		throw new vscExceptionUnimplemented ('Property [' . gettype($this) .'::' . $sVarName .'] not implemented for writing.');
+		throw new vscExceptionUnimplemented ('Property [' . get_class($this) .'::' . $sVarName .'] not implemented for writing.');
 	}
 }
