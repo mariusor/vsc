fc1863e4eb72fd4e13d866d8188535b7d2e205ef
Marius Orcsik <marius@habarnam.ro>
2010-04-01 18:22:23 +0200

    * making the folder in which we need to add templates the default one for the view

--- res/templates/rss/content.php
+++ res/templates/rss/content.php
@@ -6,7 +6,7 @@
 			<description><![CDATA[ This is the default RSS template from the <?php echo vsc::name(); ?> framework.<br/>
 In order to add content, you need to:
 <ol>
-	<li>add the <strong>rss</strong> folder in your module's template folder: <pre><?php echo $this->getMap()->getTemplatePath(); ?></pre></li>
+	<li>add the <strong><?php echo $this->getViewFolder() ?></strong> folder in your module's template folder: <pre><?php echo $this->getMap()->getTemplatePath(); ?></pre></li>
 	<li>add the default template <pre><?php echo $this->getTemplate(); ?></pre></li>
 </ol>
 These settings are defined in one of the following files:
