c7ddd53b16d351cb466500e8f203869e302cafa5
Marius Orcsik <marius@habarnam.ro>
2010-04-05 11:24:04 -0400

    * changes to the sql model...

--- res/domain/models/vscsqlmodela.class.php
+++ res/domain/models/vscsqlmodela.class.php
@@ -36,4 +36,38 @@ abstract class vscSqlModelA extends vscEmptyModel {
 	abstract public function getDatabaseUser();
 	abstract public function getDatabasePassword();
 	abstract public function getDatabaseName();
+
+    public function getDomainObjects () {
+        $oRef = new ReflectionClass($this);
+        $aProperties = $oRef->getProperties(ReflectionProperty::IS_PUBLIC);
+        foreach ($aProperties as $oProperty) {
+            if (vscEntityA::isValid ($oProperty)) {
+                $aRet[$oProperty->getName()] = $oProperty->getValue($this); 
+            }
+        }
+        return $aRet;
+    }
+
+    public function addJoin (vscEntityA $oRightObj, vscFieldA $oRightField, vscEntityA $oLeftObj, vscFieldA $oLeftField) {
+
+    }
+
+	/**
+	 *
+	 * @param vscEntityA $oChild
+	 * @return bool
+	 */
+	public function loadChild (vscEntityA $oChild) {}
+
+	/**
+	 * @todo Finish IT !!
+	 * @param vscEntityA $oChild
+	 * @return bool
+	 */
+	public function join (vscEntityA $oObject) {
+		$this->addFields ($oObject->getFields (), $oObject->getTableName());
+
+		return $this;
+	}
 }
+
