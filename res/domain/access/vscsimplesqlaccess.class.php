87cf944f81d41e2abcf590d4b2de01b0f2e46c1e
Marius Orcsik <marius@habarnam.ro>
2010-05-21 18:04:52 +0200

    * changes to the simplesql stack to allow a couple more operations

--- res/domain/access/vscsimplesqlaccess.class.php
+++ res/domain/access/vscsimplesqlaccess.class.php
@@ -31,7 +31,14 @@ class vscSimpleSqlAccess extends vscSimpleSqlAccessA implements vscSqlAccessI {
 		/* @var $oField vscFieldA */
 		foreach ($oDomainObject->getFields() as $oField) {
 			if (!is_null($oField->getValue())) {
-				$aWheres[] = $oDomainObject->getTableAlias() . '.' . $oField->getName() . ' = ' . $this->getConnection()->escape($oField->getValue());
+				$mValue		=  $this->getConnection()->escape($oField->getValue());
+				if (is_string($mValue)) {
+					// this should be moved to the sql driver
+					$sCondition = $this->getConnection()->STRING_OPEN_QUOTE . $mValue . $this->getConnection()->STRING_CLOSE_QUOTE;
+				} elseif (is_numeric($mValue) || is_null($mValue)) {
+					$sCondition = $mValue;
+				}
+				$aWheres[]	= $oDomainObject->getTableAlias() . '.' . $oField->getName() . ' = ' . $sCondition;
 			}
 		}
 
@@ -56,6 +63,41 @@ class vscSimpleSqlAccess extends vscSimpleSqlAccessA implements vscSqlAccessI {
 
 		$sRet = $this->getConnection()->_SELECT ($this->getFieldsForSelect($oDomainObject)) . $this->getConnection()->_FROM($oDomainObject->getTableName()) . $oDomainObject->getTableAlias() ."\n";
         $sRet .= $this->getConnection()->_WHERE($this->getDefaultWhereClauses($oDomainObject));
+		return $sRet;
+	}
+
+	/**
+	 * Outputs the SQL necessary for creating the table
+	 * @return string
+	 */
+	public function outputCreateTableSQL (vscDomainObjectI $oInc) {
+		$sRet = $this->getConnection()->_CREATE ($oInc->getTableName()) . "\n";
+		$sRet .= ' ( ' . "\n";
+
+		/* @var $oColumn vscFieldA */
+		foreach ($oInc->getFields () as $oColumn) {
+			$sRet .= "\t" . $oColumn->getName() . ' ' . $oColumn->getDefinition() ;
+			$sRet .= ', ' . "\n";
+		}
+
+		$aIndexes = $oInc->getIndexes(true);
+		if (is_array ($aIndexes) && !empty($aIndexes)) {
+			foreach ($aIndexes as $oIndex) {
+				if (vscIndexA::isValid($oIndex)) {
+				// this needs to be replaced with connection functionality : something like getConstraint (type, columns)
+					$sRet .=  "\t" . $oIndex->getDefinition() . ", \n"; //getType() . ' KEY ' . $oIndex->getName() . '  (' . $oIndex->getIndexComponents(). '), ' . "\n";
+				}
+			}
+		}
+
+		$sRet = substr( $sRet, 0, -3 );
+
+		$sRet.= "\n" . ' ) ';
+
+		if ($this->oConnection->getType() == sqlFactory::mysql) {
+			$sRet.= ' ENGINE ' . $this->getConnection()->getEngine();
+		}
+
 		return $sRet;
 	}
 
--- res/domain/models/vscsimplesqlmodela.class.php
+++ res/domain/models/vscsimplesqlmodela.class.php
@@ -6,10 +6,9 @@
  * @date 10.01.03
  */
 
+import ('domain/exceptions');
 import ('domain/domain');
-import ('domain/access');
 import ('domain/models');
-import ('domain/access/sqldrivers');
 
 abstract class vscSimpleSqlModelA extends vscModelA implements vscDomainObjectI {
 	private $oConnection;
@@ -37,7 +36,7 @@ abstract class vscSimpleSqlModelA extends vscModelA implements vscDomainObjectI 
 
 	public function __set ($sVarName, $sValue) {
 		try {
-			return $this->getDomainObject()->__set($sVarName, $sValue);
+			return $this->getDomainObject()->$sVarName = $sValue;
 		} catch (Exception $e) {
 			return parent::__set($sVarName, $sValue);
 		}
@@ -50,9 +49,11 @@ abstract class vscSimpleSqlModelA extends vscModelA implements vscDomainObjectI 
 			$sProperty 	= $found[2];
 
 			$sProperty[0] = strtolower ($sProperty[0]); // lowering the first letter
-		}
 
-		$oProperty = $this->getDomainObject()->__get($sProperty);
+			$oProperty = $this->getDomainObject()->__get($sProperty);
+		} else {
+			$sMethod = $sMethodName;
+		}
 
 		if ( $sMethod == 'set' && vscFieldA::isValid($oProperty)) {
 			// check for aFields with $found[1] sTableName
@@ -60,19 +61,21 @@ abstract class vscSimpleSqlModelA extends vscModelA implements vscDomainObjectI 
 			return true;
 		} else if ( $sMethod == 'get' ) {
 			return $oProperty;
+		} else {
+			return $this->getDomainObject()->$sMethod ($aParameters[0]);
 		}
-
-		return parent::__call($sMethodName, $aParameters);
+		return parent::__call($sMethod, $aParameters);
 	}
 
 	public function getConnection () {
 		return $this->oConnection;
 	}
 
-	public function __init(){
-	}
+	public function __init() {}
 
-//	abstract protected function buildObject();
+	protected function buildObject() {
+		$this->getDomainObject()->buildObject();
+	}
 
 	abstract public function getDatabaseType();
 	abstract public function getDatabaseHost();
@@ -80,7 +83,7 @@ abstract class vscSimpleSqlModelA extends vscModelA implements vscDomainObjectI 
 	abstract public function getDatabasePassword();
 	abstract public function getDatabaseName();
 
-//	abstract public function getDomainObject();
+	public function getDomainObject() {}
 
 	public function getTableName() {}
 
@@ -127,11 +130,73 @@ abstract class vscSimpleSqlModelA extends vscModelA implements vscDomainObjectI 
 		$this->oConnection = $oConnection;
 	}
 
-	public function getById ($iId) {
+	public function loadByFilter ($aFieldsArray) { // this shold be moved to the composite model
+		$aRet = array();
+		$this->getDomainObject()->fromArray ($aFieldsArray);
+
 		$a = new vscSimpleSqlAccess();
 		$a->setConnection($this->getConnection());
 
-		$this->setId($iId);
+		$this->getConnection()->query($a->outputSelectSql($this->getDomainObject()));
+
+		foreach ($this->getConnection()->getArray() as $aValues) {
+			$aObj 	= $this->getDomainObject();
+			$aObj->fromArray($aValues);
+			$aRet[] = $aObj;
+		}
+
+		return $aRet;
+	}
+
+	/**
+	 *
+	 * This has the only advantage over loadByFilter to ensure that the result returns a single entry
+	 * @param array $aFieldsArray
+	 * @throws vscExceptionDomain
+	 * @returns vscDomainObjectA
+	 */
+	public function getByUniqueIndex ($aFieldsArray) {
+		$bValid = false;
+		$aFieldNames = array_keys($aFieldsArray);
+
+		// tries to find a unique index of the entity which has values and selects an entry based on it
+		// it will find at least the primary key
+		$aIndexes = $this->getDomainObject()->getIndexes(true);
+		/* @var $oIndex vscKeyUnique */
+		foreach ($aIndexes as $oIndex) {
+			if ($oIndex->getType() & vscIndexType::UNIQUE == vscIndexType::UNIQUE) {
+				$aIndexFields 		= $oIndex->getFields();
+				$aIndexFieldNames	= array_keys($aIndexFields);
+
+				// setting the value of each field of the index
+				if ($aIndexFieldNames == $aFieldNames) {
+					foreach ($aIndexFields as $sFieldName => $oField) {
+						$oField->setValue($aFieldsArray[$sFieldName]);
+					}
+					$bValid = true;
+				}
+			}
+		}
+
+		if ($bValid) {
+			$a = new vscSimpleSqlAccess();
+			$a->setConnection($this->getConnection());
+			$sSql = $a->outputSelectSql($this->getDomainObject());
+
+			$this->getConnection()->query($sSql);
+			return $this->getDomainObject()->fromArray($this->getConnection()->getAssoc());
+		} else {
+			throw new vscExceptionDomain('None of the object unique indexes has all the neccessary values to get an unique instance.');
+		}
+	}
+
+	public function getByPrimaryKey () {
+		$a = new vscSimpleSqlAccess();
+		$a->setConnection($this->getConnection());
+
+		if ($this->getDomainObject()->hasPrimaryKey()) {
+			$this->getDomainObject();
+		}
 
 		$this->getConnection()->query($a->outputSelectSql($this->getDomainObject()));
 
