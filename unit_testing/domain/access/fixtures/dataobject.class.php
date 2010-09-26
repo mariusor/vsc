0c2ec0463b6b3abd43e132f4a00944417943a748
marius orcsik <marius@habarnam.ro>
2010-09-26 22:12:57 +0300

    - removed some unit tests that were related to simpler-sql branch

--- unit_testing/domain/access/fixtures/dataobject.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * mock object for testing the abstract fooTdoA
- */
-class vscTdo extends vscAccessA {
-	public function __construct () {
-		$this->setConnection(sqlFactory::connect('mysql'));
-	}
-}
