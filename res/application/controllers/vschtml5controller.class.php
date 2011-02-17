45247d388148b75be23cb2f37893217cff1a00da
marius orcsik <marius@habarnam.ro>
2011-02-17 22:56:48 +0200

    + added html5 view,controller and templates

---
+++ res/application/controllers/vschtml5controller.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package vsc_res_presentation
+ * @subpackage controllers
+ * @author marius orcsik <marius@habarnam.ro>
+ * @date 10.10.15
+ */
+
+import ('application/controllers');
+import ('presentation/views');
+
+class vscHtml5Controller extends vscXhtmlController {
+	public function getDefaultView () {
+		return new vscHtml5View();
+	}
+}
---
+++ res/templates/html5/main.php
@@ -0,0 +1,76 @@
+<?php /* @var $this vscHtmlView */ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+<?php
+if (count($this->getMetaHeaders()) >= 1) {
+	foreach ($this->getMetaHeaders() as $sName => $sValue) { ?>
+	<meta <?php echo 'name="'.$sName .'" content="'.$sValue.'"'; ?> />
+<?php
+	}
+}
+
+$aAllScripts = $this->getScripts(true);
+if (count ($aAllScripts) >= 1 ) {
+	foreach ($aAllScripts as $sPath) {
+?>
+	<script type="text/javascript" src="<?php echo $sPath?>"> </script>
+<?php
+	}
+}
+
+$aAllStyles = $this->getStyles ();
+if (count($aAllStyles) >= 1) {
+	foreach ($aAllStyles as $sMedia => $aStyles) {
+		if (is_array($aStyles)) {
+			foreach ($aStyles as $sPath ) {
+				echo "\t".'<link rel="stylesheet" href="' . $sPath . '"' . ($sMedia ? ' media="'. $sMedia.'"' : '') . " />\n";
+			}
+		}
+	}
+}
+
+if (is_array ($this->getLinks()) && count($this->getLinks()) >= 1) {
+	foreach ($this->getLinks() as $sType => $aLinkContent) {
+		foreach ($aLinkContent as $aValue) {
+			echo "\t".'<link type="' . $sType .'" ';
+			foreach ($aValue as $sKey => $sValue) {
+				echo $sKey . '="' . $sValue . '" ';
+			}
+			echo '/>'."\n";
+		}
+	}
+}
+?>
+	<title><?php $sTitle = $this->getTitle(); echo ($sTitle ? $sTitle : '[null]') ?></title>
+</head>
+<body>
+	<!-- hic sunt leones -->
+
+<?php
+try {
+	$sContent = $this->fetch ($this->getTemplate());
+} catch (vscExceptionPath $e) {
+	// the template could not be found
+}
+	if (!empty($sContent)) {
+		echo $sContent;
+	} else {
+		echo $this->fetch(dirname(__FILE__) . '/content.php');
+	}
+?>
+
+	<!-- /hic sunt leones -->
+<?php
+$aAllScripts = $this->getScripts();
+if (count ($aAllScripts) >= 1 ) {
+	foreach ($aAllScripts as $sPath) {
+?>
+<script type="text/javascript" src="<?php echo $sPath;?>"> </script>
+<?php
+	}
+}
+?>
+</body>
+</html>
