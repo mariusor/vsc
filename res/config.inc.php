116fea663034ebff095326b4014a0c1e8d2b6839
Marius Orcsik <marius@habarnam.ro>
2011-03-21 21:05:03 +0200

    * small changes to change where we define the library's path

--- res/config.inc.php
+++ res/config.inc.php
@@ -1,8 +1,4 @@
 <?php
-if (!defined ('VSC_PATH')) {
-	define ('VSC_PATH', realpath(dirname(__FILE__) . '/..') . DIRECTORY_SEPARATOR);
-}
-
 if (!defined ('VSC_LIB_PATH'))
 	define ('VSC_LIB_PATH', VSC_PATH . 'lib' . DIRECTORY_SEPARATOR);
 
--- vsc.inc.php
+++ vsc.inc.php
@@ -9,9 +9,12 @@ if ((int)substr($sVersion, 0, 1) < 5) {
 		throw new Exception ($sMessage);
 	}
 }
+if (!defined ('VSC_PATH')) {
+	define ('VSC_PATH', realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
+}
 
-include ('res/config.inc.php');
-include ('res/functions.inc.php');
+require (VSC_PATH . 'res'. DIRECTORY_SEPARATOR .'config.inc.php');
+require (VSC_PATH . 'res'. DIRECTORY_SEPARATOR .'functions.inc.php');
 
 import (VSC_LIB_PATH);
 import (VSC_RES_PATH);
