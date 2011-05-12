57376dde8865a667aea3031d022f2561d68ca1ae
Marius Orcsik <marius.orcsik@avangate.com>
2011-05-12 15:07:39 +0300

    * making the vsc.inc.php behave nicely when PHP version does not correspond

--- vsc.inc.php
+++ vsc.inc.php
@@ -1,8 +1,4 @@
 <?php
-$sVersion = phpversion();
-$iMajorVersion = (int)substr($sVersion, 0, 1);
-$iMinorVersion = (int)substr($sVersion, 2, 1);
-
 if (!defined ('VSC_PATH')) {
 	define ('VSC_PATH', realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
 }
@@ -14,15 +10,19 @@ if (!defined ('ROOT_MAIL')) {
 require (VSC_PATH . 'res'. DIRECTORY_SEPARATOR .'config.inc.php');
 require (VSC_PATH . 'res'. DIRECTORY_SEPARATOR .'functions.inc.php');
 
-import (VSC_LIB_PATH);
-import (VSC_RES_PATH);
-
-// including the infrastructure folder
-import (VSC_RES_PATH . 'infrastructure');
-require ('vsc.class.php');
+$sVersion = phpversion();
+$iMajorVersion = (int)substr($sVersion, 0, 1);
+$iMinorVersion = (int)substr($sVersion, 2, 1);
 
 if ($iMajorVersion < 5 || $iMinorVersion < 3) {
 	$sMessage = 'libVSC requires PHP version >= 5.3. Your current version is: ' . $sVersion;
 
 	throw new ErrorException ($sMessage, E_USER_ERROR);
+} else {
+	import (VSC_LIB_PATH);
+	import (VSC_RES_PATH);
+	
+	// including the infrastructure folder
+	import (VSC_RES_PATH . 'infrastructure');
+	require ('vsc.class.php');
 }
