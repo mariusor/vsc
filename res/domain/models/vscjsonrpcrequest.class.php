c6ebf0590f36093711d854975eee241aac67952b
Marius Orcsik <marius@habarnam.ro>
2012-08-25 16:21:33 +0300

    + adding models for requests/responses of a json-rpc implementation

---
+++ res/domain/models/vscjsonrpcrequest.class.php
@@ -0,0 +1,14 @@
+<?php
+class vscJsonRPCRequest extends vscModelA {
+	public $id = null;
+	public $method = '';
+	public $params = array();
+
+	public function __construct () {
+		$oRequest = vsc::getEnv()->getHttpRequest();
+
+		$this->id		= $oRequest->getVar ('id');
+		$this->method	= $oRequest->getVar ('method');
+		$this->params	= $oRequest->getVar ('params');
+	}
+}
