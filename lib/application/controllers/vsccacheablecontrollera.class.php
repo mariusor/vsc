df2328719f366acebc0d6aa3b8f67a13c9559eb8
Marius Orcsik <marius@habarnam.ro>
2012-12-25 21:46:40 +0200

    Insignificant changes

--- lib/application/controllers/vsccacheablecontrollera.class.php
+++ lib/application/controllers/vsccacheablecontrollera.class.php
@@ -17,24 +17,39 @@ abstract class vscCacheableControllerA extends vscFrontControllerA {
 		$oResponse = parent::getResponse($oRequest, $oProcessor);
 
 		$iNow = time();
-		$iExpireTime = 60; // one minute
+		$iExpireTime = 600; // ten minute
+		$oNow = new DateTime('now',  new DateTimeZone('GMT'));
+		$oResponse->setDate($oNow->format('r'));
 
-		if (vscCacheableViewA::isValid ($this->getView())) {
-			$iExpireTime = 604800; // one week
-			$iLastModified = strtotime($this->getView()->getModel()->modified);
+		if (vscCacheableModelA::isValid ($this->getView()->getModel())) {
+			$sLastModified = $this->getView()->getModel()->getLastModified();
+			if (!empty ($sLastModified)) {
+				$oLastModified = new DateTime($sLastModified,  new DateTimeZone('GMT'));
+				$oResponse->setLastModified($oLastModified->format('r'));
+				$oMax = $oLastModified->getTimestamp() > $oNow->getTimestamp() ? $oLastModified : $oNow;
 
+				$sModifiedSince = $oRequest->getIfModifiedSince();
+				if (!empty ($sModifiedSince)) {
+					$oModifiedSince =  new DateTime($sModifiedSince, new DateTimeZone('GMT'));
+
+					//$this->getView()->getModel()->content = $oLastModified->getTimestamp() . ' = ' . $oModifiedSince->getTimestamp();
+					if ($oLastModified->getTimestamp() <= $oModifiedSince->getTimestamp()) {
+						$oResponse->setStatus(304);
+					}
+				}
+			} else {
+				$oMax = $oNow;
+			}
+			$oResponse->setExpires($oMax->add(new DateInterval('P2W'))->format('r')); // adding 2 weeks
+		} else {
+			$oResponse->setETag( substr(sha1(serialize($this->getView()->getModel())),0,8) );
 			$oResponse->setCacheControl ('public, max-age='. $iExpireTime);
-			$oResponse->setLastModified(strftime('%a, %d %b %Y %T GMT', $iLastModified));
-			$oResponse->setExpires(strftime('%a, %d %b %Y %T GMT', max($iLastModified, $iNow) + $iExpireTime));
 
-			if ( $iLastModified > strtotime($oRequest->getIfModifiedSince()) ) {
+			if ( $oRequest->getIfNoneMatch() == '"'.$oResponse->getETag().'"' ) {
 				$oResponse->setStatus(304);
 			}
 		}
-		$oResponse->setETag(substr(sha1(serialize($this->getView()->getModel())),0,8));
-		if ( $oRequest->getIfNoneMatch() == '"'.$oResponse->getETag().'"' ) {
-			$oResponse->setStatus(304);
-		}
+
 		return $oResponse;
 	}
 }
