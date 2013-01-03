6efdb88b17c9a9c6e8574f17ba72b182adfdce13
Marius Orcsik <marius.orcsik@avangate.com>
2013-01-03 17:46:06 +0200

    Added a new method to processors to allow delegation between them

--- lib/application/processors/vscprocessora.class.php
+++ lib/application/processors/vscprocessora.class.php
@@ -106,4 +106,29 @@ abstract class vscProcessorA extends vscObject implements vscProcessorI {
 
 		return $oNewProcessor->handleRequest($oHttpRequest);
 	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see vscProcessorI::delegateRequest()
+	 */
+	public function delegateRequest(vscHttpRequestA $oHttpRequest, vscProcessorA $oNewProcessor, vscHttpResponseA $oResponse = null) {
+		$oDispatcher = vsc::getEnv()->getDispatcher();
+		$oMap = $oDispatcher->getSiteMap()->findProcessorMap($oNewProcessor);
+		
+		$oNewProcessor->setMap($oMap);
+		$oNewProcessor->init();
+		
+		/* @var $oDispatcher vscRwDispatcher */
+		$oMap->merge ($this->getMap());
+
+		if (vscHttpResponse::isValid($oResponse)) {
+			$oMap->setResponse($oResponse);
+		}
+		
+		$this->setMap ($oMap);
+		
+		$this->setLocalVars($oNewProcessor->getLocalVars());
+		
+		return $oNewProcessor->handleRequest($oHttpRequest);
+	}
 }
