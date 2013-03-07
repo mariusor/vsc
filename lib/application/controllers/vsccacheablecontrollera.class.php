1422299e1ffe30fa813c4674cbf682be015695e3
Marius Orcsik <marius.orcsik@avangate.com>
2013-03-07 19:18:18 +0200

    Improving the reponse with methods which tell of its success status
    Fixing some bugs in the front controller, when returning a model instead of a response
    
    Fixing the cacheable controller to discard caches for redirect and error responses
    
    Adding a method to tell us if the session was started

--- lib/application/controllers/vsccacheablecontrollera.class.php
+++ lib/application/controllers/vsccacheablecontrollera.class.php
@@ -21,13 +21,13 @@ abstract class vscCacheableControllerA extends vscFrontControllerA implements vs
 	 */
 	public function getResponse (vscHttpRequestA $oRequest, $oProcessor = null) {
 		$oResponse = parent::getResponse($oRequest, $oProcessor);
-
+		
 		if ( !($oResponse->isRedirect() || $oResponse->isError()) ) {
 			$iNow = time();
 			$iExpireTime = 600; // ten minute
 			$oNow = new DateTime('now',  new DateTimeZone('GMT'));
 			$oResponse->setDate($oNow->format('r'));
-
+	
 			$oModel = $this->getView()->getModel();
 			if ( vscCacheableModelA::isValid ($oModel) ) {
 				$sLastModified = $this->getView()->getModel()->getLastModified();
@@ -35,11 +35,11 @@ abstract class vscCacheableControllerA extends vscFrontControllerA implements vs
 					$oLastModified = new DateTime($sLastModified,  new DateTimeZone('GMT'));
 					$oResponse->setLastModified($oLastModified->format('r'));
 					$oMax = $oLastModified->getTimestamp() > $oNow->getTimestamp() ? $oLastModified : $oNow;
-
+	
 					$sModifiedSince = $oRequest->getIfModifiedSince();
 					if (!empty ($sModifiedSince)) {
 						$oModifiedSince =  new DateTime($sModifiedSince, new DateTimeZone('GMT'));
-
+	
 						if ($oLastModified->getTimestamp() <= $oModifiedSince->getTimestamp()) {
 							$oResponse->setStatus(304);
 						}
@@ -51,7 +51,7 @@ abstract class vscCacheableControllerA extends vscFrontControllerA implements vs
 			} else {
 				$oResponse->setETag( substr(sha1($oResponse->getOutput()), 0, 8) );
 				$oResponse->setCacheControl ('public, max-age='. $iExpireTime);
-
+	
 				if ( $oRequest->getIfNoneMatch() == '"'.$oResponse->getETag().'"' ) {
 					$oResponse->setStatus(304);
 				}
