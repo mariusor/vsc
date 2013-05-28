6d1b8b8ef4012d80c33f0ba71c74adcd901e0870
Marius Orcsik <marius@habarnam.ro>
2013-05-29 00:09:59 +0300

    Improvements to the url parser

--- unit_testing/infrastructure/urls/vscurlrwparser.stest.php
+++ unit_testing/infrastructure/urls/vscurlrwparser.stest.php
@@ -20,8 +20,24 @@ class vscUrlRWParserTest extends Snap_UnitTestCase {
 		return $this->assertTrue($oUrl->isLocal());
 	}
 
+	public function testIsRemote() {
+		$oUrl = new vscUrlRWParser('goole.com');
+		return $this->assertFalse($oUrl->isLocal());
+	}
+
+	public function testIsRemoteIP() {
+		$oUrl = new vscUrlRWParser('8.8.8.8');
+		return $this->assertFalse($oUrl->isLocal());
+	}
+
+	public function testNoSchemeIP() {
+		$oUrl = new vscUrlRWParser('//8.8.8.8');
+		return $this->assertTrue($oUrl->getCompleteUri(true) == 'http://8.8.8.8');
+	}
+
 	public function testUrlNoSchemePath () {
 		$oUrl = new vscUrlRWParser('//localhost');
 		return $this->assertTrue($oUrl->getCompleteUri(true) == 'http://localhost');
 	}
+
 }
