66f5a6840e843fbc86074455348bb5a878274d44
Marius Orcsik <marius@habarnam.ro>
2013-06-01 14:04:18 +0300

    Adding baseEncode/baseDecode functions to my string class

--- res/infrastructure/vscstring.class.php
+++ res/infrastructure/vscstring.class.php
@@ -73,4 +73,25 @@ class vscString {
 			return $sString;
 		}
 	}
+
+	static public function baseEncode($val, $base=62, $chars='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') {
+		// can't handle numbers larger than 2^31-1 = 2147483647
+		$str = '';
+		do {
+			$i = $val % $base;
+			$str = $chars[$i] . $str;
+			$val = ($val - $i) / $base;
+		} while($val > 0);
+		return $str;
+	}
+
+	static public function baseDecode($str, $base=62, $chars='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') {
+		$len = strlen($str);
+		$val = 0;
+		$arr = array_flip(str_split($chars));
+		for($i = 0; $i < $len; ++$i) {
+			$val += $arr[$str[$i]] * pow($base, $len-$i-1);
+		}
+		return $val;
+	}
 }
