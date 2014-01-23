ab3231141adcbdb3243680738ca797e5b237c997
Marius Orcsik <marius@habarnam.ro>
2014-01-23 20:46:32 +0200

    Having the output functions handle cli vs. browser usage

--- res/functions.inc.php
+++ res/functions.inc.php
@@ -102,7 +102,7 @@ function getPaths () {
 function cleanBuffers ($iLevel = null) {
 	$aErrors = array();
 	$iLevel = ob_get_level();
-	for ($i = 0; $i <= 1 ; $i++) {
+	for ($i = 0; $i < min(1, $iLevel) ; $i++) {
 		ob_end_clean();
 	}
 
@@ -209,24 +209,26 @@ function getDirFiles ( $dir, $showHidden = false){
 
 if (!function_exists('_e')) {
 	function getErrorHeaderOutput ($e = null) {
-		header ('HTTP/1.1 500 Internal Server Error');
-		$sRet = '<?xml version="1.0" encoding="utf-8"?>';
-		$sRet .= '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"';
-		$sRet .= '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
-		$sRet .= '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">';
-		$sRet .= '<head>';
-		$sRet .= '<style>ul {padding:0; font-size:0.8em} li {padding:0.2em;display:inline} address {position:fixed;bottom:0;}</style>';
-		$sRet .= '<title>Internal Error' . (!$e ? '' : ': '. substr($e->getMessage(), 0, 20) . '...') . '</title>';
-		$sRet .= '</head>';
-		$sRet .= '<body>';
-		$sRet .= '<strong>Internal Error' . (!$e ? '' : ': '. $e->getMessage()) . '</strong>';
-		$sRet .= '<address>&copy; VSC</address>';
-		$sRet .= '<ul><li><a href="#" onclick="p = document.getElementById(\'trace\'); if (p.style.display==\'block\') p.style.display=\'none\';else p.style.display=\'block\'; return false">toggle trace</a></li><li><a href="javascript: p = document.getElementById(\'trace\'); document.location.href =\'mailto:'.ROOT_MAIL.'?subject=Problems&amp;body=\' + p.innerHTML; return false">mail me</a></li></ul>';
+		if (!isCli()) {
+			header ('HTTP/1.1 500 Internal Server Error');
+			$sRet = '<?xml version="1.0" encoding="utf-8"?>';
+			$sRet .= '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"';
+			$sRet .= '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
+			$sRet .= '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">';
+			$sRet .= '<head>';
+			$sRet .= '<style>ul {padding:0; font-size:0.8em} li {padding:0.2em;display:inline} address {position:fixed;bottom:0;}</style>';
+			$sRet .= '<title>Internal Error' . (!($e instanceof Exception) ? '' : ': '. substr($e->getMessage(), 0, 20) . '...') . '</title>';
+			$sRet .= '</head>';
+			$sRet .= '<body>';
+			$sRet .= '<strong>Internal Error' . (!($e instanceof Exception) ? '' : ': '. $e->getMessage()) . '</strong>';
+			$sRet .= '<address>&copy; VSC</address>';
+			$sRet .= '<ul><li><a href="#" onclick="p = document.getElementById(\'trace\'); if (p.style.display==\'block\') p.style.display=\'none\';else p.style.display=\'block\'; return false">toggle trace</a></li><li><a href="javascript: p = document.getElementById(\'trace\'); document.location.href =\'mailto:'.ROOT_MAIL.'?subject=Problems&amp;body=\' + p.innerHTML; return false">mail me</a></li></ul>';
 
-		if ($e instanceof Exception)
-			$sRet .= '<p style="font-size:.8em">Triggered in <strong>' . $e->getFile() . '</strong> at line ' . $e->getLine() .'</p>';
+			if ($e instanceof Exception)
+				$sRet .= '<p style="font-size:.8em">Triggered in <strong>' . $e->getFile() . '</strong> at line ' . $e->getLine() .'</p>';
 
-		$sRet .= '<pre style="position:fixed;bottom:2em;display:none;font-size:.8em" id="trace">';
+			$sRet .= '<pre style="position:fixed;bottom:2em;display:none;font-size:.8em" id="trace">';
+		}
 
 		return $sRet;
 	}
@@ -234,18 +236,34 @@ if (!function_exists('_e')) {
 	function _e ($e) {
 		$aErrors = cleanBuffers();
 
-		header ('HTTP/1.1 500 Internal Server Error');
-		echo getErrorHeaderOutput ($e);
-		if (isDebug()) {
-			echo $e ? $e->getTraceAsString() : '';
+		if (!isCli()) {
+			header ('HTTP/1.1 500 Internal Server Error');
+			echo getErrorHeaderOutput ($e);
 		}
+
+		if ( isDebug() ) {
+			echo ($e instanceof Exception) ? $e->getTraceAsString() : '';
+		}
+
 		if (count($aErrors) > 0) {
-			echo '<h2>Previous Errors</h2>';
-			echo '<p>' . implode ($aErrors, '<br/>') . '</p>';
+			if (!isCli()) { echo '<h2>'; }
+			echo 'Previous Errors';
+			if (!isCli()) { echo '</h2>'; }
+			if (!isCli()) { echo '<p>'; }
+			echo implode ($aErrors, isCli() ? "\n" : '<br/>');
+			if (!isCli()) { echo '</p>'; }
 		}
-		echo '</pre>';
-		echo '</body>';
-		echo '</html>';
+
+		if (!isCli()) {
+			echo '</pre>';
+			echo '</body>';
+			echo '</html>';
+		} else {
+			if ($e instanceof Exception) {
+				$e->getMessage() . "\n";
+				$sRet .= "\t". $e->getFile() . ' at line ' . $e->getLine() . "\n";
+			}
+		}
 		exit (0);
 	}
 }
