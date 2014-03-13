8cc25a244814599d7a693c911c311bc47de9365d
Marius Orcsik <marius@habarnam.ro>
2014-03-13 16:39:36 +0200

    Removed phpunit related configuration, as I started to use the distro's package

--- unit_testing/vsc_phpunittest_environment.php
+++ unit_testing/vsc_phpunittest_environment.php
@@ -2,22 +2,22 @@
 /**
  * @package unit_tests
  * @subpackage PHPUnit
- * 
- * Bolierplate code to make PHPUnit play nice with vsc * 
+ *
+ * Bolierplate code to make PHPUnit play nice with vsc *
  */
 
 if (!defined('VSC_PATH')) {
 	define ( 'VSC_PATH', realpath( dirname (__FILE__) . '/../') . DIRECTORY_SEPARATOR );
 	set_include_path (VSC_PATH . PATH_SEPARATOR . get_include_path());
 	require ('vsc.inc.php');
-	
+
 	error_reporting(E_ALL);
 	ini_set('display_errors', '1');
 }
 
-if (!defined('PHPUNIT_PATH')) {
-	define ('PHPUNIT_PATH', '/usr/share/pear/PHPUnit/');
-	set_include_path (PHPUNIT_PATH . PATH_SEPARATOR . get_include_path());
-	require (PHPUNIT_PATH . 'Autoload.php');
-}
+//if (!defined('PHPUNIT_PATH')) {
+//	define ('PHPUNIT_PATH', '/usr/share/pear/PHPUnit/');
+//	set_include_path (PHPUNIT_PATH . PATH_SEPARATOR . get_include_path());
+//	require (PHPUNIT_PATH . 'Autoload.php');
+//}
 
