ec69a15e7aca9fd19890a41ff152ae3b01a1e7e9
Marius Orcsik <marius@habarnam.ro>
2014-07-26 18:36:30 +0300

    Added a cli dispatcher

---
+++ lib/application/dispatchers/vscclidispatchera.class.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Parses the current request into a valid Front Controller / Controller pair
+ * @package vsc_application
+ * @subpackage dispatchers
+ * @author marius orcsik <marius@habarnam.ro>
+ * @date 14.07.26
+ */
+
+abstract class vscCLIDispatcherA extends vscDispatcherA {
+}
