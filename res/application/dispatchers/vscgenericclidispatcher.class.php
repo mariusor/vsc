d91a0c1e073742ae992d535ed64d6d4d2179365d
Marius Orcsik <marius@habarnam.ro>
2014-07-26 18:59:07 +0300

    Completing the ignored model tests

---
+++ res/application/dispatchers/vscgenericclidispatcher.class.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Parses the current request into a valid Front Controller / Controller pair
+ * @package vsc_application
+ * @subpackage dispatchers
+ * @author marius orcsik <marius@habarnam.ro>
+ * @date 14.07.26
+ */
+
+class vscGenericCLIDispatcher extends vscCLIDispatcherA {
+	/**
+	 * @return vscFrontControllerA
+	 */
+	public function getFrontController () {
+		// TODO: Implement getFrontController() method.
+	}
+
+	/**
+	 * @return vscProcessorA
+	 */
+	public function getProcessController () {
+		// TODO: Implement getProcessController() method.
+	}
+
+	public function getView () {
+		// TODO: Implement getView() method.
+	}
+
+	/**
+	 * @param string $sIncPath
+	 * @throws vscExceptionPath
+	 * @return void
+	 */
+	public function loadSiteMap ( $sIncPath ) {
+		// TODO: Implement loadSiteMap() method.
+	}
+} 
