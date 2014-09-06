a714656e1bf9748b25d7a6dc7215d152c2d5b588
Marius Orcsik <marius@habarnam.ro>
2014-09-06 17:58:36 +0300

    Moving the processor and controller mapping functionality to traits
    They are now being used also in the ClassMap object

---
+++ lib/application/sitemaps/ControllerMapT.php
@@ -0,0 +1,119 @@
+<?php
+namespace vsc\application\sitemaps;
+
+use vsc\ExceptionPath;
+use vsc\presentation\views\ViewA;
+
+trait ControllerMapT {
+	private $sMainTemplatePath;
+	private $sMainTemplate;
+
+	private $sViewPath;
+	private $oView;
+
+	abstract public function getModuleMap();
+
+	/**
+	 * @param string $sPath
+	 * @throws \vsc\ExceptionPath
+	 */
+	public function setMainTemplatePath ($sPath) {
+		$sMainTemplatePath = realpath($sPath);
+		if ( !is_dir($sMainTemplatePath) ) {
+			$sMainTemplatePath = realpath($this->getModuleMap()->getModulePath() . DIRECTORY_SEPARATOR . $sPath);
+		}
+		if ( !is_dir($sMainTemplatePath) ) {
+			throw new ExceptionPath (sprintf('Path [%s] does not exist', $sPath));
+		}
+		$this->sMainTemplatePath = $sMainTemplatePath;
+
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getMainTemplatePath () {
+		// if we didn't provide the controller with a main template path we check the module
+		if ( is_null($this->sMainTemplatePath) ) {
+			if ( $this->getModuleMap() instanceof ContentTypeMappingI) {
+				$this->sMainTemplatePath = $this->getModuleMap()->getMainTemplatePath();
+			}
+		}
+
+		if ( is_null($this->sMainTemplatePath) ) {
+			// back-up
+			$this->sMainTemplatePath = VSC_RES_PATH . 'templates';
+		}
+		if ( substr($this->sMainTemplatePath, 0, -1) != DIRECTORY_SEPARATOR ) {
+			$this->sMainTemplatePath .= DIRECTORY_SEPARATOR;
+		}
+		return $this->sMainTemplatePath;
+	}
+
+	/**
+	 * @param string $sPath
+	 */
+	public function setMainTemplate ($sPath) {
+		$this->sMainTemplate = $sPath;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getMainTemplate () {
+		// if we didn't provide the controller with a main template path we check the module
+		if ( is_null($this->sMainTemplate ) ) {
+			if ( $this->getModuleMap() instanceof ContentTypeMappingI) {
+				$this->sMainTemplate = $this->getModuleMap()->getMainTemplate();
+			}
+		}
+		if ( is_null($this->sMainTemplate)) {
+			// back-up
+			$this->sMainTemplate = 'main.php';
+		}
+		return $this->sMainTemplate;
+	}
+
+	/**
+	 *
+	 * To allow a single controller type to return a different type of view
+	 * @param string|object $mView
+	 * @throws ExceptionPath
+	 */
+	public function setView ($mView) {
+		if (ViewA::isValid($mView)) {
+			$this->oView = $mView;
+		} elseif (stristr(basename($mView), '.') === false && !is_file($mView)) {
+			// namespaced class name
+			$this->sViewPath = $mView;
+		} elseif (SiteMapA::isValidObjectPath($mView)) {
+			$this->sViewPath = $mView;
+		} else {
+			throw new ExceptionPath ('View path [' . $mView . '] is not valid.');
+		}
+	}
+
+	public function getViewPath() {
+		return $this->sViewPath;
+	}
+
+	public function getView() {
+		if ( !ViewA::isValid($this->oView) && !is_null($this->sViewPath)){
+			if (stristr(basename($this->sViewPath), '.') === false && !is_file($this->sViewPath)) {
+				$sClassName = $this->sViewPath;
+			} elseif (is_file($this->sViewPath)) {
+				$sViewPath = $this->getViewPath();
+				try {
+					include ($sViewPath);
+				} catch (\Exception $e) {
+					\vsc\_e($e);
+				}
+				$sClassName = SiteMapA::getClassName($sViewPath);
+			}
+		}
+		if (!empty($sClassName)) {
+			$this->oView = new $sClassName();
+		}
+		return $this->oView;
+	}
+} 
--- res/application/sitemaps/ProcessorMap.php
+++ res/application/sitemaps/ProcessorMap.php
@@ -7,45 +7,10 @@
  */
 namespace vsc\application\sitemaps;
 
-use vsc\presentation\helpers\ViewHelperA;
-use vsc\presentation\requests\HttpRequestA;
-use vsc\presentation\responses\HttpResponseA;
-use vsc\presentation\responses\HttpResponseType;
-
 class ProcessorMap extends MappingA {
-	/**
-	 *
-	 * @var HttpResponseA
-	 */
-	private $oResponse;
+	use ProcessorMapT;
 
 	/**
-	 *
-	 * @var ViewHelperA[]
-	 */
-	private $aHelpers = array();
-
-	/**
-	 * @var int
-	 */
-	private $iHttpStatus;
-
-	/**
-	 * @param HttpResponseA $oResponse
-	 */
-	public function setResponse (HttpResponseA $oResponse) {
-		$this->oResponse = $oResponse;
-	}
-
-	/**
-	 * @returns HttpResponseA
-	 */
-	public function getResponse () {
-		return $this->oResponse;
-	}
-
-	/**
-	 *
 	 * @param string $sRegex
 	 * @param string $sPath
 	 * @throws ExceptionSitemap
@@ -58,36 +23,5 @@ class ProcessorMap extends MappingA {
 			$sRegex = $this->getRegex();
 		}
 		return parent::mapController($sRegex, $sPath);
-	}
-
-	/**
-	 * @param ViewHelperA $oHelper
-	 * @return void
-	 */
-	public function registerHelper (ViewHelperA $oHelper) {
-		$this->aHelpers[] = $oHelper;
-	}
-
-	/**
-	 * @returns ViewHelperA[]
-	 */
-	public function getViewHelpers () {
-		return $this->aHelpers;
-	}
-
-	/**
-	 * @param int $iStatus
-	 */
-	public function setResponseStatus ($iStatus) {
-		if (HttpResponseType::isValidStatus($iStatus)) {
-			$this->iHttpStatus = $iStatus;
-		}
-	}
-
-	/**
-	 * @return int
-	 */
-	public function getResponseStatus () {
-		return $this->iHttpStatus;
 	}
 }
