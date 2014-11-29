09e962891a0ea650f781e8a9238983b4185e7a8e
Marius Orcsik <marius@habarnam.ro>
2014-11-29 15:22:44 +0200

    Converting the fixture map to use ::class

--- tests/fixtures/config/map.php
+++ tests/fixtures/config/map.php
@@ -2,7 +2,6 @@
 
 /* @var $this \vsc\application\sitemaps\RwSiteMap */
 $this->getCurrentModuleMap()->setTemplatePath(VSC_FIXTURE_PATH . 'templates' . DIRECTORY_SEPARATOR );
-$oMap = $this->map ('test', '\\fixtures\\application\\processors\\ProcessorFixture');
-
-$oMap->mapController('.*', '\\fixtures\\application\\controllers\\GenericFrontController');
+$oMap = $this->map ('test', \fixtures\application\processors\ProcessorFixture::class);
+$oMap->mapController('.*', \fixtures\application\controllers\GenericFrontController::class);
 
