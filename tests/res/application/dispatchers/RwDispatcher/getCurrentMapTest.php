574647bffeae74537c234e2ecf2355ba53972e62
Marius Orcsik <marius@habarnam.ro>
2015-01-04 13:44:43 +0100

    Added RwDispatcher::getCurrentMap tests

--- tests/res/application/dispatchers/RwDispatcher/getCurrentMapTest.php
+++ tests/res/application/dispatchers/RwDispatcher/getCurrentMapTest.php
@@ -1,13 +1,63 @@
 <?php
 namespace tests\res\application\dispatchers\RwDispatcher;
+use vsc\application\dispatchers\RwDispatcher;
+use vsc\application\sitemaps\ClassMap;
+use vsc\application\sitemaps\MappingA;
+use vsc\application\processors\EmptyProcessor;
 
 /**
  * @covers \vsc\application\dispatchers\RwDispatcher::getCurrentMap()
  */
 class getCurrentMap extends \PHPUnit_Framework_TestCase
 {
-	public function testIncomplete()
+	public function testCurrentMapWithEmptyMaps()
 	{
-		$this->markTestIncomplete(" ... ");
+		$aMaps = array();
+
+		$o = new RwDispatcher();
+		$oCurrentMap = $o->getCurrentMap($aMaps);
+		$this->assertInstanceOf(MappingA::class, $oCurrentMap);
+		$this->assertInstanceOf(ClassMap::class, $oCurrentMap);
+		$this->assertEquals('', $oCurrentMap->getRegex());
+		$this->assertEquals('', $oCurrentMap->getPath());
+	}
+
+	public function testCurrentMapWithEnvRequest()
+	{
+		$sRegex = '.*';
+		$aMaps = array();
+		$aMaps[$sRegex] = new ClassMap(EmptyProcessor::class, $sRegex);
+
+		$o = new RwDispatcher();
+		$oCurrentMap = $o->getCurrentMap($aMaps);
+		$this->assertInstanceOf(MappingA::class, $oCurrentMap);
+		$this->assertInstanceOf(ClassMap::class, $oCurrentMap);
+		$this->assertEquals($sRegex, $oCurrentMap->getRegex());
+		$this->assertEquals(EmptyProcessor::class, $oCurrentMap->getPath());
+	}
+
+	public function testCurrentMapWithInvalidRegex()
+	{
+		set_error_handler(
+			function ($iSeverity, $sMessage, $sFilename, $iLineNo) {
+				\vsc\exceptions_error_handler($iSeverity, $sMessage, $sFilename, $iLineNo);
+			}
+		);
+
+		$sRegex = '\\';
+		$aMaps = array();
+		$aMaps[$sRegex] = new ClassMap(EmptyProcessor::class, $sRegex);
+
+		$o = new RwDispatcher();
+		try {
+			$o->getCurrentMap($aMaps);
+		} catch (\ErrorException $err) {
+			$errMessage =<<<START
+preg_match_all(): No ending delimiter '#' found<br/> Offending regular expression: <span style="font-weight:normal">#\#iu</span>
+START;
+
+			$this->assertInstanceOf(\vsc\ExceptionError::class, $err);
+			$this->assertEquals($errMessage, $err->getMessage());
+		}
 	}
 }
