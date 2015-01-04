5ab7e300e7f5189553225c0a6645371b36cad459
Marius Orcsik <marius@habarnam.ro>
2015-01-04 10:19:50 +0100

    Moved the HttpResponseType unit-tests

--- tests/lib/presentation/responses/HttpResponseType/getStatusTest.php
+++ tests/lib/presentation/responses/HttpResponseType/getStatusTest.php
@@ -1,13 +1,20 @@
 <?php
 namespace tests\lib\presentation\responses\HttpResponseType;
+use vsc\presentation\responses\HttpResponseType;
 
 /**
  * @covers \vsc\presentation\responses\HttpResponseType::getStatus()
  */
 class getStatus extends \PHPUnit_Framework_TestCase
 {
-	public function testIncomplete()
-	{
-		$this->markTestIncomplete(" ... ");
+	public function testGetStatus() {
+		$Mirror = new \ReflectionClass(HttpResponseType::class);
+		$MirrorProperty = $Mirror->getProperty('aStatusList');
+		$MirrorProperty->setAccessible(\ReflectionProperty::IS_PUBLIC);
+		$aStatuses = $MirrorProperty->getValue();
+
+		foreach ($aStatuses as $iStatus => $sStatusText) {
+			$this->assertEquals($aStatuses[$iStatus], HttpResponseType::getStatus($iStatus));
+		}
 	}
 }
--- tests/lib/presentation/responses/HttpResponseType/isValidStatusTest.php
+++ tests/lib/presentation/responses/HttpResponseType/isValidStatusTest.php
@@ -1,13 +1,25 @@
 <?php
 namespace tests\lib\presentation\responses\HttpResponseType;
+use vsc\presentation\responses\HttpResponseType;
 
 /**
  * @covers \vsc\presentation\responses\HttpResponseType::isValidStatus()
  */
 class isValidStatus extends \PHPUnit_Framework_TestCase
 {
-	public function testIncomplete()
+	public function testBasicIsValidStatus ()
 	{
-		$this->markTestIncomplete(" ... ");
+		$Mirror = new \ReflectionClass(HttpResponseType::class);
+		$MirrorProperty = $Mirror->getProperty('aStatusList');
+		$MirrorProperty->setAccessible(\ReflectionProperty::IS_PUBLIC);
+		$aStatuses = $MirrorProperty->getValue();
+
+		for ($i = 0; $i < 600; $i++) {
+			if (array_key_exists($i, $aStatuses)) {
+				$this->assertTrue(HttpResponseType::isValidStatus($i));
+			} else {
+				$this->assertFalse(HttpResponseType::isValidStatus($i));
+			}
+		}
 	}
 }
