5ef84f4dabf65fc85b21193d2be91ff31dc24650
Marius Orcsik <marius@habarnam.ro>
2015-01-20 15:49:18 +0100

    Unit-test for HttpRequestA::getFile

--- tests/lib/presentation/requests/HttpRequestA/getFileTest.php
+++ tests/lib/presentation/requests/HttpRequestA/getFileTest.php
@@ -1,13 +1,18 @@
 <?php
 namespace tests\lib\presentation\requests\HttpRequestA;
+use vsc\presentation\requests\HttpRequestA;
 
 /**
  * @covers \vsc\presentation\requests\HttpRequestA::getFile()
  */
 class getFile extends \PHPUnit_Framework_TestCase
 {
-	public function testIncomplete()
+	public function testEmptyAtInitialize()
 	{
-		$this->markTestIncomplete(" ... ");
+		$o = new HttpRequestA_underTest_getFile();
+		$this->assertEmpty($o->getFile('test'));
 	}
+
 }
+
+class HttpRequestA_underTest_getFile extends HttpRequestA {}
