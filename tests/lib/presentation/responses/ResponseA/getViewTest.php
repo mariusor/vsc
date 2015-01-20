f79d029df7a3556515c85f8f6e3a36fe6d183f61
Marius Orcsik <marius@habarnam.ro>
2015-01-20 15:29:20 +0100

    Added unit-tests for ResponseA class

--- tests/lib/presentation/responses/ResponseA/getViewTest.php
+++ tests/lib/presentation/responses/ResponseA/getViewTest.php
@@ -1,13 +1,62 @@
 <?php
 namespace tests\lib\presentation\responses\ResponseA;
+use vsc\presentation\responses\ResponseA;
+use vsc\infrastructure\Null;
+use vsc\presentation\views\ViewA;
 
 /**
  * @covers \vsc\presentation\responses\ResponseA::getView()
  */
 class getView extends \PHPUnit_Framework_TestCase
 {
-	public function testIncomplete()
+	public function testEmptyAtInitialization()
 	{
-		$this->markTestIncomplete(" ... ");
+		$o = new ResponseA_underTest_getView();
+		$this->assertInstanceOf(Null::class, $o->getView());
+	}
+
+	public function testBasicGetView ()
+	{
+		$o = new ResponseA_underTest_getView();
+
+		$o->setView(new ViewA_underTest_getView());
+		$this->assertInstanceOf(ViewA::class, $o->getView());
+		$this->assertInstanceOf(ViewA_underTest_getView::class, $o->getView());
+	}
+}
+
+class ResponseA_underTest_getView extends ResponseA {}
+
+class ViewA_underTest_getView extends ViewA {
+	/**
+	 * appends values to template variables
+	 *
+	 * @param array|string $tpl_var the template variable name(s)
+	 * @param mixed $value the value to append
+	 */
+	function append($tpl_var, $value = null, $merge = false)
+	{
+		// TODO: Implement append() method.
+	}
+
+	/**
+	 * assigns values to template variables
+	 *
+	 * @param array|string $tpl_var the template variable name(s)
+	 * @param mixed $value the value to assign
+	 */
+	function assign($tpl_var, $value = null)
+	{
+		// TODO: Implement assign() method.
+	}
+
+	/**
+	 * executes & displays the template results
+	 *
+	 * @param string $resource_name
+	 */
+	function display($resource_name)
+	{
+		// TODO: Implement display() method.
 	}
 }
--- tests/lib/presentation/responses/ResponseA/setViewTest.php
+++ tests/lib/presentation/responses/ResponseA/setViewTest.php
@@ -1,13 +1,21 @@
 <?php
 namespace tests\lib\presentation\responses\ResponseA;
+use vsc\presentation\responses\ResponseA;
+use vsc\presentation\views\JsonView;
 
 /**
  * @covers \vsc\presentation\responses\ResponseA::setView()
  */
 class setView extends \PHPUnit_Framework_TestCase
 {
-	public function testIncomplete()
+	public function testBasicSetView()
 	{
-		$this->markTestIncomplete(" ... ");
+		$o = new ResponseA_underTest_setView();
+		$oView = new JsonView();
+		$o->setView($oView);
+
+		$this->assertSame ($oView, $o->getView());
 	}
 }
+
+class ResponseA_underTest_setView extends ResponseA {}
