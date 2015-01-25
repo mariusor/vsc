a5bd934be461c85db38030f01ac085a6e39179bc
Marius Orcsik <marius@habarnam.ro>
2015-01-25 16:21:02 +0100

    Unit-tests for ExceptionResponseError

--- tests/lib/presentation/responses/ExceptionResponseError/__constructTest.php
+++ tests/lib/presentation/responses/ExceptionResponseError/__constructTest.php
@@ -1,5 +1,7 @@
 <?php
 namespace tests\lib\presentation\responses\ExceptionResponseError;
+use vsc\presentation\responses\ExceptionResponseError;
+use vsc\presentation\responses\HttpResponseType;
 
 /**
  * @covers \vsc\presentation\responses\ExceptionResponseError::__construct()
@@ -8,6 +10,11 @@ class __construct extends \PHPUnit_Framework_TestCase
 {
 	public function testIncomplete()
 	{
-		$this->markTestIncomplete(" ... ");
+		$sMessage = uniqid('test:');
+		$iCode = HttpResponseType::INTERNAL_ERROR;
+
+		$o = new ExceptionResponseError($sMessage, $iCode);
+		$this->assertEquals($sMessage, $o->getMessage());
+		$this->assertEquals($iCode, $o->getErrorCode());
 	}
 }
--- tests/lib/presentation/responses/ExceptionResponseError/getErrorCodeTest.php
+++ tests/lib/presentation/responses/ExceptionResponseError/getErrorCodeTest.php
@@ -1,13 +1,22 @@
 <?php
 namespace tests\lib\presentation\responses\ExceptionResponseError;
+use vsc\presentation\responses\ExceptionResponseError;
+use vsc\presentation\responses\HttpResponseType;
 
 /**
  * @covers \vsc\presentation\responses\ExceptionResponseError::getErrorCode()
  */
 class getErrorCode extends \PHPUnit_Framework_TestCase
 {
-	public function testIncomplete()
+	public function testBasticGetErrorCode()
 	{
-		$this->markTestIncomplete(" ... ");
+		$o = new ExceptionResponseError_underTest_getErrorCode(HttpResponseType::INTERNAL_ERROR);
+		$this->assertEquals(HttpResponseType::INTERNAL_ERROR, $o->getErrorCode());
+	}
+}
+
+class ExceptionResponseError_underTest_getErrorCode extends ExceptionResponseError {
+	public function __construct ($iStatus, $sMessage = 'test') {
+		parent::__construct($sMessage, $iStatus);
 	}
 }
