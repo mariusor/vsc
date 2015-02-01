7dfb725d95d5630237f5bb91d76abb3f2cb43ae0
Marius Orcsik <marius@habarnam.ro>
2015-02-01 19:29:54 +0100

    Unit-tests for ExceptionAuthenticationNeeded

--- tests/lib/presentation/responses/ExceptionAuthenticationNeeded/__constructTest.php
+++ tests/lib/presentation/responses/ExceptionAuthenticationNeeded/__constructTest.php
@@ -1,13 +1,23 @@
 <?php
 namespace tests\lib\presentation\responses\ExceptionAuthenticationNeeded;
+use vsc\presentation\responses\ExceptionAuthenticationNeeded;
+use vsc\presentation\responses\HttpResponseType;
 
 /**
  * @covers \vsc\presentation\responses\ExceptionAuthenticationNeeded::__construct()
  */
 class __construct extends \PHPUnit_Framework_TestCase
 {
-	public function testIncomplete()
+	public function testGetBasicChallenge()
 	{
-		$this->markTestIncomplete(" ... ");
+		$sMessage = 'test';
+		$sRealm = 'unit-test';
+		$sNonce = uniqid();
+		$o = new ExceptionAuthenticationNeeded_underTest___construct($sMessage, $sRealm);
+
+		$this->assertEquals($sMessage, $o->getMessage());
+		$this->assertEquals(HttpResponseType::NOT_AUTHORIZED, $o->getErrorCode());
 	}
 }
+
+class ExceptionAuthenticationNeeded_underTest___construct extends ExceptionAuthenticationNeeded {}
--- tests/lib/presentation/responses/ExceptionAuthenticationNeeded/getChallengeTest.php
+++ tests/lib/presentation/responses/ExceptionAuthenticationNeeded/getChallengeTest.php
@@ -1,13 +1,24 @@
 <?php
 namespace tests\lib\presentation\responses\ExceptionAuthenticationNeeded;
+use vsc\presentation\responses\ExceptionAuthenticationNeeded;
 
 /**
  * @covers \vsc\presentation\responses\ExceptionAuthenticationNeeded::getChallenge()
  */
 class getChallenge extends \PHPUnit_Framework_TestCase
 {
-	public function testIncomplete()
+	public function testGetBasicChallenge()
 	{
-		$this->markTestIncomplete(" ... ");
+		$sMessage = 'test';
+		$sRealm = 'unit-test';
+		$o = new ExceptionAuthenticationNeeded_underTest_getChallenge($sMessage, $sRealm);
+
+		$sChallenge =<<<Start
+Basic realm="{$sRealm}"
+Start;
+
+		$this->assertEquals($sChallenge, $o->getChallenge());
 	}
 }
+
+class ExceptionAuthenticationNeeded_underTest_getChallenge extends ExceptionAuthenticationNeeded {}
