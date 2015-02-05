71c14b1f08297fbb999d65b2f1a950e642738208
Marius Orcsik <marius@habarnam.ro>
2015-02-05 16:25:56 +0100

    Unit-test for ErrorControllerMap constructor

--- tests/res/application/sitemaps/ErrorControllerMap/__constructTest.php
+++ tests/res/application/sitemaps/ErrorControllerMap/__constructTest.php
@@ -1,13 +1,18 @@
 <?php
 namespace tests\res\application\sitemaps\ErrorControllerMap;
+use vsc\application\sitemaps\ErrorControllerMap;
+use vsc\application\controllers\Html5Controller;
 
 /**
  * @covers \vsc\application\sitemaps\ErrorControllerMap::__construct()
  */
 class __construct extends \PHPUnit_Framework_TestCase
 {
-	public function testIncomplete()
+	public function testBasic__construct()
 	{
-		$this->markTestIncomplete(" ... ");
+		$o = new ErrorControllerMap();
+		$this->assertEquals('\A.*\Z', $o->getRegex());
+		$this->assertEquals(Html5Controller::class, $o->getPath());
+
 	}
 }
