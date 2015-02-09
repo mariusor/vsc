afc3edfbf3fe0610fcdcc752d45bf413002aeb23
Marius Orcsik <marius@habarnam.ro>
2015-02-09 22:50:18 +0100

    JsonRPCResponse getProperties unit-test

--- tests/res/domain/models/JsonRPCResponse/getPropertiesTest.php
+++ tests/res/domain/models/JsonRPCResponse/getPropertiesTest.php
@@ -1,5 +1,6 @@
 <?php
 namespace tests\res\domain\models\JsonRPCResponse;
+use vsc\domain\models\JsonRPCResponse;
 
 /**
  * @covers \vsc\domain\models\JsonRPCResponse::getProperties()
@@ -8,6 +9,18 @@ class getProperties extends \PHPUnit_Framework_TestCase
 {
 	public function testIncomplete()
 	{
-		$this->markTestIncomplete(" ... ");
+		$jsonRPC = [
+			'id' => null,
+			'result' => null,
+			'error' => null
+		];
+		$o = new JsonRPCResponse_underTest_getProperties();
+		$this->assertEquals($jsonRPC, $o->getProperties());
+	}
+}
+
+class JsonRPCResponse_underTest_getProperties extends JsonRPCResponse {
+	public function getProperties ($bIncludeNonPublic = false) {
+		return parent::getProperties($bIncludeNonPublic);
 	}
 }
