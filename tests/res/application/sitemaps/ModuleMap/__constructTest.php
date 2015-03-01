99fe9e7d0ecdec835ea9fa573030d60538d1c296
Marius Orcsik <marius@habarnam.ro>
2015-03-02 00:12:40 +0100

    Dunno what happened

--- tests/res/application/sitemaps/ModuleMap/__constructTest.php
+++ tests/res/application/sitemaps/ModuleMap/__constructTest.php
@@ -1,20 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: habarnam
- * Date: 3/1/15
- * Time: 11:58 PM
- */
-
 namespace res\application\sitemaps\ModuleMap;
-
 
 use vsc\application\sitemaps\ModuleMap;
 
+/**
+ * Class __constructTest
+ * @package res\application\sitemaps\ModuleMap
+ * @covers \vsc\application\sitemaps\ModuleMap::__construct()
+ */
 class __constructTest extends \PHPUnit_Framework_TestCase {
 
 	public function testInitialization () {
-		= '';
+		$r = '';
 		$o = new ModuleMap(VSC_FIXTURE_PATH . 'config/map.php', $r);
 		$this->assertEquals(VSC_FIXTURE_PATH, $o->getPath());
 		$this->assertEquals($r, $o->getRegex());
