85e500e14847b8db7d26718fef745710751c0f4f
Marius Orcsik <marius@habarnam.ro>
2015-04-14 14:22:53 +0200

    Added a new test for ProcessorMap::mapController method

--- tests/res/application/sitemaps/ProcessorMap/mapControllerTest.php
+++ tests/res/application/sitemaps/ProcessorMap/mapControllerTest.php
@@ -10,9 +10,22 @@ class mapController extends \PHPUnit_Framework_TestCase
 {
 	public function testBasicMapController ()
 	{
+		$regex = '.*';
 		$o = new ProcessorMap(__FILE__, '.*');
-		$map = $o->mapController('.*', __FILE__);
+		$map = $o->mapController($regex, __FILE__);
 
 		$this->assertInstanceOf(MappingA::class, $map);
+		$this->assertEquals($regex, $map->getRegex());
+		$this->assertEquals(__FILE__, $map->getPath());
+	}
+
+	public function testMapControllerWithNoRegex ()
+	{
+		$o = new ProcessorMap(__FILE__, '.*');
+		$map = $o->mapController(__FILE__);
+
+		$this->assertInstanceOf(MappingA::class, $map);
+		$this->assertEquals($o->getRegex(), $map->getRegex());
+		$this->assertEquals(__FILE__, $map->getPath());
 	}
 }
