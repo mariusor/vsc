7b42e1eca94fe8da1e40e374e4ecb66110e602c5
Marius Orcsik <marius@habarnam.ro>
2017-01-26 16:40:14 +0100

    Rerun the vsc\Exception::isValid tests

--- tests/unit/lib/Exception/isValidTest.php
+++ tests/unit/lib/Exception/isValidTest.php
@@ -8,17 +8,17 @@ use vsc\Exception;
 class isValid extends \PHPUnit_Framework_TestCase
 {
 	public function testValidException() {
-//		$E = new Exception();
-//		$this->assertTrue (Exception::isValid ($E));
-//		$this->assertEquals (Exception::isValid ($E), ($E instanceof Exception));
-//		$this->assertInstanceOf(Exception::class, $E);
-//		$this->assertInstanceOf(\Exception::class, $E);
+		$E = new Exception();
+		$this->assertTrue (Exception::isValid ($E));
+		$this->assertEquals (Exception::isValid ($E), ($E instanceof Exception));
+		$this->assertInstanceOf(Exception::class, $E);
+		$this->assertInstanceOf(\Exception::class, $E);
 	}
 
 	public function testInvalidException() {
-//		$F = new \Exception();
-//		$this->assertFalse(Exception::isValid($F));
-//		$this->assertEquals(Exception::isValid($F), ($F instanceof Exception));
-//		$this->assertInstanceOf(\Exception::class, $F);
+		$F = new \Exception();
+		$this->assertFalse(Exception::isValid($F));
+		$this->assertEquals(Exception::isValid($F), ($F instanceof Exception));
+		$this->assertInstanceOf(\Exception::class, $F);
 	}
 }
